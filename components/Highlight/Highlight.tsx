94e59fd663df13d66c87dd5a089512725be257a2
Adam-pw <adampithewan@gmail.com>
2023-03-10 12:33:42 +0530

    UI changes

--- components/Highlight/Highlight.tsx
+++ components/Highlight/Highlight.tsx
@@ -11,29 +11,34 @@ export default function Highlight() {
   });
   return (
     <>
-      <div
-        className={styles.main}
-        style={{ backgroundImage: "url(/images/highlight.png)", objectFit: "cover" }}
-      >
+      <div className={styles.main}>
         <div className={styles.linear}>
           <div className={styles.card}>
             <div className={styles.creator}>
-              <Image width={24} height={24} src="/images/highcreator.png" alt="" />
+              <Image
+                width={24}
+                height={24}
+                src="/images/highcreator.png"
+                alt=""
+              />
               Shroomie
             </div>
             <div className={styles.row}>
               <div>
                 <div className={styles.head}>Magic Mashrooms</div>
                 <button className={styles.button}>
-                  <Image width={20} height={20} src="/images/nftcards/eye.png" alt="" />
+                  <Image
+                    width={20}
+                    height={20}
+                    src="/images/nftcards/eye.png"
+                    alt=""
+                  />
                   See NFT
                 </button>
               </div>
               <div className={styles.timmer}>
                 <div className={styles.end}>Auction starts in:</div>
-                <div className={styles.number}>
-                  {time.slice(0,-3)}
-                </div>
+                <div className={styles.number}>{time.slice(0, -3)}</div>
                 <div className={styles.unit}>
                   <div className="">Hours</div>
                   <div className="">Minutes</div>
